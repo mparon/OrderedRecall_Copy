78a8b4b21babd1d201102fd686849bf14ec3f6ac
mparon <48919326+mparon@users.noreply.github.com>
2024-12-10 13:57:22 -0500

    added practice file

---
+++ static/js/pregenerated_sessions/1/practice.js
@@ -0,0 +1,14 @@
+var pregenerated_practice_lists = [
+	{
+		conditions: {pos: "sequential", max_list: "16"},
+		words: ["tweezers", "daughter", "chipmunk", "notebook", "preacher", "princess", "cookbook", "airplane"]
+	},
+	{
+		conditions: {pos: "sequential", max_list: "16"},
+		words: ["sidewalk", "outdoors", "sculpture", "fragrance", "scribble", "trombone", "headband", "transplant", "scissors", "dandruff", "blackboard", "basement", "briefcase", "creature", "mattress", "ointment"]
+	},
+	{
+		conditions: {pos: "sequential", max_list: "16"},
+		words: ["whiskers", "wardrobe", "football", "suitcase", "backbone", "lightning", "softball", "dashboard", "lipstick", "cocktail", "boyfriend", "cupboard"]
+	},
+]
