6be3d455a1a926a05392973243d94ad274851aa7
mparon <48919326+mparon@users.noreply.github.com>
2024-12-10 14:10:16 -0500

    added 99 and practice files

---
+++ list_gen/OrderedRecallLists/1/99.js
@@ -0,0 +1,113 @@
+var pregenerated_practice_lists = [
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["goal", "pail", "bridge", "vault", "spark", "shell", "tail", "beak", "wave", "dock", "tent", "book"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["hill", "nail", "chalk", "maze", "stake", "teeth", "steak", "spear"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["sheet", "sun", "worm", "fur", "chief", "cup", "farm", "cave", "nest", "cheek", "fence", "web", "pea", "sign", "mug", "food"]
+	},
+]
+
+var pregenerated_lists = [
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["peach", "space", "judge", "couch", "turtle", "blender", "drug", "donkey", "maple", "agent", "slide", "tutu", "sticker", "razor", "guitar", "coffee"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["bike", "human", "igloo", "sunrise", "bacon", "record", "worker", "jelly"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["mustard", "infant", "slope", "honey", "orange", "clam", "snake", "friend", "camel", "office", "canal", "whistle"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["item", "waiter", "package", "inmate", "clamp", "glass", "salmon", "partner"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["quarter", "sausage", "puddle", "proton", "cook", "apple", "loft", "report", "cobra", "ketchup", "cabin", "pilot", "omelet", "panther", "slime", "arm"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["balloon", "shark", "banjo", "crutch", "woman", "japan", "lily", "trout", "crown", "rug", "soda", "burglar"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["toaster", "peanut", "hand", "ghetto", "bank", "zebra", "coin", "crib", "london", "trench", "highway", "hiker", "kitchen", "switch", "rattle", "cigar"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["radish", "asia", "padding", "pimple", "mop", "flask", "baby", "bison", "thief", "flower", "banker", "shrub"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["valve", "nomad", "tribe", "hood", "salt", "seagull", "critic", "rifle"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["custard", "beast", "scotch", "drill", "vagrant", "powder", "knife", "washer"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["climber", "smog", "ulcer", "primate", "lumber", "ankle", "mammal", "plaster", "rabbit", "author", "dolphin", "clock"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["queen", "lover", "saddle", "cyclone", "frame", "scalpel", "seal", "keeper", "crab", "table", "lapel", "luggage", "velvet", "robin", "perch", "binder"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["tunnel", "ozone", "france", "dancer", "husband", "termite", "beef", "fruit"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["witness", "treat", "permit", "lake", "marker", "island", "laser", "parent", "bug", "canyon", "actress", "elbow"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["bathtub", "jungle", "slug", "perfume", "gymnast", "palace", "yarn", "passage", "pork", "journal", "convict", "gallon", "hatchet", "fungus", "pasta", "cod"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["yacht", "cashew", "pastry", "spider", "plaza", "drink", "ground", "finger", "apron", "patrol", "curtain", "lace"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["denim", "relish", "lettuce", "penguin", "flag", "chapel", "freezer", "venus"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["heart", "tuba", "leopard", "mailbox", "fleet", "uncle", "mailman", "staple", "salad", "flipper", "hamper", "buggy", "garden", "nurse", "freckle", "icing"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["award", "barley", "pecan", "sunset", "garlic", "forest", "rooster", "guard", "marine", "wagon", "twig", "lady"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["leader", "sketch", "atom", "sphinx", "robber", "racket", "catcher", "tart"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["spoon", "lamp", "cocoon", "hornet", "cross", "fox", "cashier", "mars", "hedge", "brandy", "river", "garbage", "cousin", "orchid", "bullet", "casket"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["globe", "supper", "stove", "wrist", "gift", "message", "poet", "hawk", "motor", "blade", "survey", "postage", "jello", "twister", "blouse", "chest"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["donor", "wife", "servant", "dress", "popcorn", "glove", "diver", "missile"]
+	},
+	{
+		conditions: {pos: "spatial", max_list: "16"},
+		words: ["market", "banquet", "fiddle", "braces", "rod", "shirt", "lunch", "mildew", "swamp", "lion", "wrench", "flute"]
+	},
+]
