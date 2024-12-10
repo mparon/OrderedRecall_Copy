cde136322dc69c5d36a324be3c656d5c46ef8c58
mparon <48919326+mparon@users.noreply.github.com>
2024-12-10 14:08:35 -0500

    added 99 and practice files

---
+++ list_gen/OrderedRecallLists/0/99.js
@@ -0,0 +1,113 @@
+var pregenerated_practice_lists = [
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["jar", "park", "prince", "flea", "wave", "mouth", "mud", "crane", "oar", "moon", "slush", "meat", "dock", "skate", "street", "steak"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["spark", "shoe", "weed", "tree", "mug", "hole", "worm", "stair", "sock", "owl", "elf", "yolk"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["horn", "bread", "yard", "wing", "ink", "cat", "cup", "grease"]
+	},
+]
+
+var pregenerated_lists = [
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["poison", "scalpel", "shovel", "silk", "outfit", "mammal", "wrench", "belly", "heart", "picnic", "child", "atom", "razor", "lumber", "pebble", "hook"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["lunch", "husband", "clamp", "waiter", "banjo", "sister", "bunny", "crown", "finger", "college", "mailbox", "laser"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["crab", "jury", "mustard", "grave", "message", "turnip", "bubble", "balloon"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["pistol", "pecan", "robber", "forest", "star", "snack", "receipt", "tunnel", "valley", "yarn", "bullet", "japan", "ketchup", "snail", "seafood", "actress"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["money", "rifle", "girl", "canal", "france", "leader", "skirt", "costume", "cocoon", "mister", "guitar", "peach"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["uncle", "valve", "cook", "puppy", "brook", "neutron", "brandy", "curtain"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["vagrant", "dustpan", "chapel", "parrot", "marine", "lace", "cashew", "lamp", "arm", "airport", "stone", "lover"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["bug", "kleenex", "relish", "body", "beaver", "keeper", "omelet", "worker"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["rebel", "highway", "pudding", "hand", "pliers", "cousin", "tuba", "dentist", "monster", "rooster", "possum", "tablet", "canyon", "raft", "hound", "buggy"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["nurse", "plaid", "palace", "skillet", "pilot", "cobra", "egypt", "space", "infant", "thread", "switch", "item", "virus", "jello", "patient", "fleet"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["scarf", "seagull", "taxi", "office", "tutu", "garlic", "summit", "shelf", "bouquet", "marrow", "penguin", "jungle"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["nomad", "shrub", "tourist", "chemist", "shirt", "drill", "jelly", "hostess"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["lily", "spool", "pepper", "puddle", "stream", "cross", "binder", "fruit", "guard", "atlas", "butcher", "sword"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["xerox", "proton", "outlaw", "lodge", "donkey", "milk", "camel", "crib"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["shorts", "scallop", "cloud", "mask", "diver", "catcher", "foot", "rocket", "lagoon", "breast", "venus", "world", "barrel", "icing", "ground", "servant"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["passage", "dust", "igloo", "lady", "dime", "turkey", "brother", "ox"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["leg", "victim", "staple", "pasta", "dolphin", "rattle", "country", "stamp", "crayon", "robin", "novel", "salt"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["temple", "gravel", "freckle", "piston", "pigment", "kidney", "marker", "wrist", "postage", "dorm", "package", "dragon", "orange", "banquet", "church", "lake"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["viking", "trench", "blender", "hawk", "captive", "rabbit", "swamp", "braces"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["napkin", "bike", "county", "velvet", "turtle", "match", "zipper", "fireman", "coin", "globe", "treat", "missile", "student", "sparrow", "flag", "drawing"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["mildew", "wasp", "cabin", "spider", "teacher", "mummy", "termite", "cashier", "grape", "motel", "mailman", "rust"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["freezer", "quail", "snake", "washer", "citrus", "clock", "flipper", "tribe", "supper", "typist", "blade", "baby", "journal", "drug", "luggage", "perch"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["pigeon", "picture", "paddle", "tractor", "tile", "widow", "police", "queen", "cabbage", "cottage", "comet", "gallon"]
+	},
+	{
+		conditions: {pos: "temporal", max_list: "16"},
+		words: ["mars", "ghetto", "soda", "knife", "plaza", "liver", "london", "denim"]
+	},
+]
